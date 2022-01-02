878707c870c8f451ff0fa15f0da33cf37d536dc6
hesh-git <theshan.19@cse.mrt.ac.lk>
2022-01-02 15:58:36 +0530

    error page  styled

--- view/notFound.php
+++ view/notFound.php
@@ -11,7 +11,10 @@
     <div class="err_page_right">
         <h1>404</h1>
         <h4>OOPS. Looks like the page you're looking for no longer exists</h4>
-        <p>Don't worry. Since you're valuable to us we will bring you back to safety</p>
+        <div id="err">
+            <h6 >Don't worry. Since you're valuable to us we will bring you back to safety</h6>
+        </div>
+
         <div class="button">
             <a class="err_btn" href="<?php echo "http://localhost:8080/"?>">Back to home</a>
         </div>
